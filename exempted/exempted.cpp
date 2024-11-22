076f2cbc7752476cd2b6412469478ba45397b231
fortski <202410143@fit.edu.ph>
2024-11-22 10:39:07 +0800

    conditional

---
+++ exempted/exempted.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int age;
+	cout << "How old are you? ";
+	cin >> age;
+
+	
+	cout << "This is your age: " << age << "\n";
+	return 0;
+}
